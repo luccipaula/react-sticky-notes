deb9c777682971b085b5baa29bfb47a820491149
paulacaixeta <vivaluccistudio@gmail.com>
2018-10-09 17:48:12 -0700

    First commit, adding project to repo

---
+++ src/Board.js
@@ -0,0 +1,116 @@
+import React, { Component } from 'react'
+import Note from './Note'
+import FaPlus from 'react-icons/lib/fa/plus'
+
+class Board extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            notes: [
+                {
+                    id: 0,
+                    note: "Call Merlin"
+                },
+                {
+                    id: 1,
+                    note: "Learn React"
+                },
+                {
+                    id: 2,
+                    note: "Walk the doggo"
+                },
+                {
+                    id: 3,
+                    note: "Apply for jobs"
+                },
+                {
+                    id: 4,
+                    note: "Buy more ramen"
+                },
+                {
+                    id: 5,
+                    note: "Design business cards"
+                },
+                {
+                    id: 6,
+                    note: "Do yoga"
+                },
+                {
+                    id: 7,
+                    note: "Update this app"
+                },
+                {
+                    id: 8,
+                    note: "Push this to GitHub"
+                },
+                {
+                    id: 9,
+                    note: "Be happy"
+                },
+            ]
+        }
+        this.add = this.add.bind(this)
+        this.eachNote = this.eachNote.bind(this)
+        this.update = this.update.bind(this)
+        this.remove = this.remove.bind(this)
+        this.nextId = this.nextId.bind(this)
+    }
+
+    add(text) {
+        this.setState(prevState => ({
+            notes: [
+                ...prevState.notes,
+                {
+                    id: this.nextId(),
+                    note: text
+                }
+            ]
+        }))
+    }
+
+    nextId() {
+        this.uniqueId = this.uniqueId || 0
+        return this.uniqueId++
+    }
+
+    update(newText, i) {
+		console.log('updating item at index', i, newText)
+		this.setState(prevState => ({
+			notes: prevState.notes.map(
+				note => (note.id !== i) ? note : {...note, note: newText}
+			)
+		}))
+	}
+
+    remove(id) {
+        console.log('removing item at', id)
+        this.setState(prevState => ({
+            notes: prevState.notes.filter(note => note.id !== id)
+        }))
+    }
+
+    eachNote(note, i) {
+        return (
+            <Note key={note.id}
+                  index={note.id}
+                  onChange={this.update}
+                  onRemove={this.remove}>
+                  {note.note}
+            </Note>
+        )
+    }
+
+    render() {
+        return (
+            <div className="board">
+                {this.state.notes.map(this.eachNote)}
+                <button onClick={this.add.bind(null, "New Note")}
+                        id="add">
+                    <FaPlus />
+                </button>
+            </div>
+        )
+    }
+}
+
+export default Board
